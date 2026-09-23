dfc7e517c32c2329e38aae24c6e7ef653594ce59
sid19b <boinisiddharth19@gmail.com>
2026-09-23 19:34:39 +0530

    add incident post api enpoint

--- ai-service/app/main.py
+++ ai-service/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title="AI Kubernetes Incident Platform")
 
+
+class Incident(BaseModel):
+    name: str
+    namespace: str
+    resource: str
+    resource_type: str
+    reason: str
 
 @app.get("/")
 def root():
@@ -16,3 +24,22 @@ def health():
     return {
         "status": "healthy"
     }
+
+@app.post("/incidents")
+def create_incident(incident: Incident):      
+
+#     incident = Incident(
+#     name="payment-api-crash",
+#     namespace="dev",
+#     reason="CrashLoopBackOff"
+# )
+    return {
+        "incident": incident.name,
+        "namespace": incident.namespace,
+        "resource": incident.resource,
+        "resource_type": incident.resource_type,
+        "reason": incident.reason,
+        "status": "received"
+    }
+
+                                                    
